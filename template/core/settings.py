24edc6f907b9529417dac689d018025df0302e29
arumia <arumia@knu.ac.kr>
2021-11-15 23:06:57 +0900

    지도api 적용 및 도메인 적용, 날씨 동기화 및 기타등등 수정

--- template/core/settings.py
+++ template/core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sami.6nights.tech', config('SERVER', default='192.168.0.11')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sami.works', config('SERVER', default='192.168.0.11')]
 
 # Application definition
 
