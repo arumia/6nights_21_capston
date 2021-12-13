c1e4f8604d306b1eff5d41c86f0c0638db686337
arumia <arumia@knu.ac.kr>
2021-12-13 21:23:31 +0900

    test

--- template/app/views.py
+++ template/app/views.py
@@ -61,17 +61,7 @@ def rfid(request):
         if not error:
             print("Tag detected")
             (error, uid) = rdr.anticoll()
-            if not error:
-                print("UID: " + str(uid))
-                # Select Tag is required before Auth
-                if not rdr.select_tag(uid):
-                    # Auth for block 10 (block 2 of sector 2) using default shipping key A
-                    if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
-                        # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-                        print("Reading block 10: " + str(rdr.read(10)))
-                        # Always stop crypto1 when done working
-                        rdr.stop_crypto()
-
+            print("UID: " + str(uid))
     # Calls GPIO cleanup
     rdr.cleanup()
     return JsonResponse(str(uid))
