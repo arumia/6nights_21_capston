15e1b0658201e264093dfac7ec31a9979b5e3b8c
arumia <arumia@knu.ac.kr>
2021-12-13 14:06:02 +0900

    셀러리 적용중

--- template/core/__init__.py
+++ template/core/__init__.py
@@ -4,6 +4,6 @@ Copyright (c) 2019 - present AppSeed.us
 """
 # 이렇게 설정해주면 Django가 시작할떄 Celery가 항상 import 되고
 # shared_task 데코레이션이 Celery를 이용하게됩니다.
-from template.core.celery import app as celery_app
+from .celery import app as celery_app
 
 __all__ = ('celery_app',)
