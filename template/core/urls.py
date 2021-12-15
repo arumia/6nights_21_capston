537f1f4aadda3ff311401082a4f1cce801014327
arumia <arumia@knu.ac.kr>
2021-12-15 14:56:30 +0900

    db 테스트

--- template/core/urls.py
+++ template/core/urls.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include  # add this
 from rest_framework import routers
-from app.views import WeatherViewSet
+from app.views import WeatherViewSet, WorkViewSet
 
 router = routers.DefaultRouter()
 router.register('weather', WeatherViewSet) # prefix = movies , viewset = MovieViewSet
