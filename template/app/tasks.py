793d3055cc6b046f7a891d510e39bc8fb2bab16f
arumia <arumia@knu.ac.kr>
2022-01-03 20:36:44 +0900

    도커파일 및 기타 수정

--- template/app/tasks.py
+++ template/app/tasks.py
@@ -1,3 +1,6 @@
+from celery import shared_task
+
+@shared_task
 def getgps():
     import gpsd
 
---
+++ template/djnago_start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2020, ROBOTIS CO., LTD.
+
+echo ""
+echo "장고 서버 실행 스크립트"
+echo "[Note] 0.1ver"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+echo "[Set the target ROS version and name of colcon workspace]"
+source venv/bin/activate
+
+echo "[Set Djnago]"
+python3 manage.py makemigrations app
+python3 manage.py migrate
+
+echo "[Start Djnago]"
+python3 manage.py runserver sami.works:8000 &
+
+echo "[Start Selery]"
+celery -A core worker -l info -B &
+
+echo "[Start Selery]"
+python3 ../tornado/server.py &
+
+echo "[Complete!!!]"
+exit 0
